d7d07d1fac53a70d5a9905219d7a7ac0f9c75a4a
louis70109 <louis70109@gmail.com>
2021-05-25 00:49:47 +0800

    change test case name

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_main2():
+def test_health_check():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World!"}
